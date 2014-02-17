5ed68ad95eddeb91456bcf22300b0b718aa479e6
Mark Tertishniy <mtertishniy@gmail.com>
2014-02-17 23:00:43 +1100

    Add time editing

---
+++ scripts/handlers/handler.Time.php
@@ -0,0 +1,24 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/scripts/classes/class.Time.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/scripts/handlers/handler.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/scripts/utils.php';
+
+class TimeHandler extends Handler
+{
+   public function __construct()
+   {
+      $this->entity = new DBTime;
+   }
+}
+
+$ajaxResult = Array('result' => true, 'message' => 'Операция прошла успешно!');
+
+try {
+   $post = GetPOST();
+   $handler = new TimeHandler();
+   $handler->Handle($post);
+} catch (Exception $e) {
+   $ajaxResult['result'] = false;
+   $ajaxResult['message'] = $e->getMessage();
+}
+echo json_encode($ajaxResult);
